16cddf8421a5f7b091714d082426b0edf0a468dc
morillas <morillas@f86dea77-7e2e-0410-97ea-a74e350978e6>
2007-05-15 09:39:19 +0000

    Contadores malos y buenos. Uso de sesiones

--- cherrypy/contador.py
+++ cherrypy/contador.py
@@ -2,21 +2,63 @@
 
 import cherrypy
 
+# variable global veces para usar en contador (malo)
+veces = 0
+
 class Contador:
+    veces = 0
     _cp_config = {'tools.sessions.on': True}
     def index(self):
+        # Muestra de dos contadores: uno malo y otro bueno.
+        return """<h1>Ejemplo de contadores</h1>
+        <p><a href="/bueno">Contador bueno (usa sesiones)</a></p>
+        <p><a href="/malo">Contador malo (usa una variable de clase)</a></p>
+        <p><a href="/otromalo">Contador malo (usa una variable global)</a></p>
+        """
+    index.exposed = True
+
+    def bueno(self):
         # Incrementa el contador cada vez que se carga la página
+        # si aún no se ha introducido la clave contador en el diccionario
+        # de la sesión, devuelve 0
         veces = cherrypy.session.get('contador', 0) + 1
         
         # almacena el nuevo valor en el diccionario session
         cherrypy.session['contador'] = veces
-        
+
+        Contador.veces += 1
         # Muestra el mensaje
         return '''
             En la sesión actual has visto la página
             %s veces.
         ''' % veces
-    index.exposed = True
+    bueno.exposed = True
+
+
+    def malo(self):
+        # Usa la variable de clase: veces
+        # Comprobar accediendo desde distintos equipos
+        Contador.veces += 1
+        # Muestra el mensaje
+        return '''
+            En la sesión actual has visto la página
+            %s veces.
+        ''' % Contador.veces
+    malo.exposed = True
+
+    def otromalo(self):
+        # Usa una variable global: veces
+        # Comprobar accediendo desde distintos equipos
+        global veces
+        veces += 1
+        # Muestra el mensaje
+        return '''
+            En la sesión actual has visto la página
+            %s veces.
+        ''' % veces
+    otromalo.exposed = True
+    
+
 
 cherrypy.quickstart(Contador())
 
