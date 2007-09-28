01a0517a450b3bf1ed3e74d94650c9c1d1770722
morillas <morillas@f86dea77-7e2e-0410-97ea-a74e350978e6>
2007-09-28 08:35:52 +0000

    Viernes 28 sept

---
+++ 0708/primeros/ejemplos_print.py
@@ -0,0 +1,10 @@
+# Ejemplo de cadenas de formato
+
+# formato para cadena -  entero - float
+# alinear cadena a la izquierda --> -
+formato = "%-10s %5d %5.2f"
+
+
+print formato % ("Ana", 1200, 5.4)
+print formato % ("Antonio", 100, 20.35)
+print formato % ("Carmen", 20, 40)
